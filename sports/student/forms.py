a6fe25e954a9ecf1daad56cf8b56f0b834e2a44c
Vignesh Hari <vichuhari100@gmail.com>
2019-02-06 15:08:41 +0530

    Finished Styling Partly

--- sports/student/forms.py
+++ sports/student/forms.py
@@ -13,4 +13,12 @@ class StudentCreationForm(forms.ModelForm):
         'name',
         )
 
+    def __init__(self, *args, **kwargs):
+        super(StudentCreationForm, self).__init__(*args, **kwargs)
+        self.fields['admission_number'].widget.attrs.update({
+                'class': 'form-control',
+            })
+        self.fields['name'].widget.attrs.update({
+                'class': 'form-control',
+            })
 
